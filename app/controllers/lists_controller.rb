1a89b645eafa9fa2fa39459558ad7c986e493d8b
Okan Erturan <okanert@gmail.com>
2021-09-10 11:04:58 +0300

    Add list form

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,9 +7,27 @@ class ListsController < ApplicationController
 
   def show; end
 
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+
+    if @list.save
+        redirect_to lists_path
+    else
+        render :new
+    end
+  end
+
   private
 
   def set_list
     @list = List.find(params[:id])
+  end
+
+  def list_params
+      params.require(:list).permit(:name)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Clearing db"
+Movie.destroy_all
+
+url = "http://tmdb.lewagon.com/movie/top_rated"
+
+movies = JSON.parse(URI.open(url).read)["results"]
+
+movies.each do |movie|
+    Movie.create!(
+        title: movie["title"],
+        overview: movie["overview"],
+        rating: movie["vote_average"],
+        poster_url: "https://image.tmdb.org/t/p/w500#{movie["poster_path"]}"
+    )
+end
+
+puts "Created #{Movie.count} movie(s)"
