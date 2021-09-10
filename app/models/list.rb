07880fce336836ba30b3288e894b795fa7e53fd4
Okan Erturan <okanert@gmail.com>
2021-09-10 15:25:46 +0300

    style improvements

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,5 @@
 class List < ApplicationRecord
+  has_one_attached :photo
   validates :name, uniqueness: true, presence: true
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
